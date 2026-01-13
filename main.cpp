9f5818e9fd817b3fece87332cecb7976d37a7cd7
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2026-01-13 08:38:16 -0500

    Add .idea configuration files for project setup

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    
+    cout << "Calculator Example" << endl;
+    cout << "What is the first number? ";
+    int num1;
+    cin >> num1;
+    cout << "What is the second number? ";
+    int num2;
+    cin >> num2;
+    cout << "The sum of " << num1 << " and " << num2 << " is " << num1 + num2 << endl;
     return 0;
 }
