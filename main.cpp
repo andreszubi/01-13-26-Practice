20232a2ed8ebffc6cb83141befca27cf2e6f047e
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2026-01-13 08:42:45 -0500

    Refactor calculator example to store sum in a variable

--- main.cpp
+++ main.cpp
@@ -2,14 +2,21 @@
 using namespace std;
 
 int main() {
-    
+
+    int num1;
+    int num2;
+    int sum;
+
     cout << "Calculator Example" << endl;
     cout << "What is the first number? ";
-    int num1;
+
     cin >> num1;
     cout << "What is the second number? ";
-    int num2;
+
     cin >> num2;
-    cout << "The sum of " << num1 << " and " << num2 << " is " << num1 + num2 << endl;
+
+    sum = num1 + num2;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
     return 0;
 }
