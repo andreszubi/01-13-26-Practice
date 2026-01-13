0f7553d1a359383a7a9efb72bcfb3722c95aa04b
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2026-01-13 08:57:50 -0500

    Add user input for first and last name in calculator example

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main() {
     int num1;
     int num2;
     int sum;
+    string firstName;
+    string lastName;
 
     cout << "Calculator Example" << endl;
     cout << "What is the first number? ";
@@ -18,5 +20,14 @@ int main() {
     sum = num1 + num2;
 
     cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+
+    cout << "Type in your first name:";
+    cin >> firstName;
+    cout << "Type in your last name:";
+    cin >> lastName;
+    cout << "Hello " << firstName << " " << lastName << endl;
+
+
     return 0;
 }
